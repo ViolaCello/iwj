8b7e66ce6a9dbca1f20de2670ab7aadb25742823
ViolaCello <ViolaCelloNYC@gmail.com>
2021-09-03 18:47:10 -0400

    Diems -> index

--- app/controllers/diems_controller.rb
+++ app/controllers/diems_controller.rb
@@ -1,2 +1,26 @@
 class DiemsController < ApplicationController
+
+    def index
+      #  raise.params.inspect
+      if logged_in?
+            @user = User.find_by(id: current_user.id)
+            @diems = @user.diems
+        else 
+            redirect_to '/'
+        end
+    end
+    
+    
+    def new
+       @user = current_user if logged_in?
+            if !!@user
+                @venues = Venue.all
+                @event = @user.events.build
+            else 
+                redirect_to '/'
+            end
+    end
+
+
+
 end
