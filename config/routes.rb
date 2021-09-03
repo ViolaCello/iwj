9063e5b763cad20af5404f28442d3aba064c6722
ViolaCello <ViolaCelloNYC@gmail.com>
2021-09-03 17:20:59 -0400

    sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  get '/', to: 'sessions#index', as: 'home'
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+
   resources :dreams
   resources :practices
   resources :diems
