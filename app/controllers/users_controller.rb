90cdd0ff938f51c93291e0f82c0794f8c6f58eb0
ViolaCello <ViolaCelloNYC@gmail.com>
2021-09-03 17:29:03 -0400

    user controller login and create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+    
+    def create
+       @user = User.create(user_params)
+       if @user.valid?
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+      else 
+        render :new
+      end
+    end
+        
+    def show
+         @user = User.find_by(id: params[:id])
+         redirect_to '/' if @user == nil
+    end
+    
+private
+
+def user_params
+    params.require(:user).permit(:name, :password, :rising)
+end
+
 end
