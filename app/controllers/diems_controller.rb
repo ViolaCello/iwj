86a5df81f623b2b2058e81541abc1f7835b76621
ViolaCello <ViolaCelloNYC@gmail.com>
2021-09-03 19:17:33 -0400

    The Diem create and show works

--- app/controllers/diems_controller.rb
+++ app/controllers/diems_controller.rb
@@ -14,13 +14,41 @@ class DiemsController < ApplicationController
     def new
        @user = current_user if logged_in?
             if !!@user
-                @venues = Venue.all
-                @event = @user.events.build
+                @diems = @user.diems.build
             else 
                 redirect_to '/'
             end
     end
 
+    def create
+        @user = current_user if logged_in?
+            if !!@user
+                @diem = @user.diems.build(diem_params)
+     
+                if @diem.save
+                    redirect_to diem_path(@diem)
+                else 
+                     @errors = @diem.errors
+                    render :'diems/new'
+                end
+            end
+    end
+
+    def show
+        @user = current_user if !!current_user
+        if !!@user 
+            @info = @user.diems.find_by(:id[params[:id]])
+        else 
+            redirect_to '/'
+        end
+    end
+
+
+    private
+
+    def diem_params
+        params.require(:diem).permit(:custdate, :moonsign, :moonstat)
+    end
 
 
 end
